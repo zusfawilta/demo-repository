ac0368a71622d59ce9d26ca35c78ed08ae1dad16
zusfawilta <zusfawilta@gmail.com>
2024-11-30 22:01:11 +0700

    Create math_util.py

---
+++ math_util.py
@@ -0,0 +1,7 @@
+def add(x, y):
+    """Adds two numbers and returns the result."""
+    return x + y
+
+def subtract(x, y):
+    """Subtracts two numbers and returns the result."""
+    return x - y
