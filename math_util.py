bc0a03292e9b7698a340f3445b6fc1a178639da8
zusfawilta <zusfawilta@gmail.com>
2024-11-30 22:31:34 +0700

    Update math_util.py

--- math_util.py
+++ math_util.py
@@ -5,3 +5,10 @@ def add(x, y):
 def subtract(x, y):
     """Subtracts two numbers and returns the result."""
     return x - y
+
+def multiply(x, y):  # New function
+    """Multiplies two numbers and returns the result."""
+    return x * y
+
+def divide(a, b):
+    return a / b
